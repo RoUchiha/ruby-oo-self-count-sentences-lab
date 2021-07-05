b198682fd73fa777adc9c45dbdbbbacbc17bc36a
RoUchiha <swaggsikh@gmail.com>
2021-07-05 00:22:30 -0500

    commit tree 4aa88ed1ea53c065a855a40042bd6bf751368803

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,39 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self[-1, 1] == "."
+      return true
+    else
+      return false
+    end
   end
 
   def question?
-
+    if self[-1, 1] == "?"
+      return true
+    else
+      return false
+    end
   end
 
   def exclamation?
-
+    if self[-1, 1] == "!"
+      return true
+    else
+      return false
+    end
   end
 
   def count_sentences
-
+    punctuation = [".", "?", "!"]
+    sentences = []
+    sentences = self.split(Regexp.union(punctuation))
+    sentences.delete_if do |sentence|
+      sentence.length < 2
+    end
+  
+    return sentences.length
+   
   end
+   
 end
